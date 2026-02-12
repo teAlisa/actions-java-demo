f2bf049ceea97206abd5ab2b32f6500bcc37c85f
teAlisa <alisateige@gmail.com>
2026-02-12 10:06:12 -0500

    Fix main method declaration in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static void min(String[] args){
-    System.out.println(x:"Hello!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
